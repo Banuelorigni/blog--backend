cb15842b33a5a6fc00600edb3de67c80f7ed20be
kexin.an <kexin.an@thoughtworks.com>
2023-03-22 16:00:25 +0800

    chore: remove unused imports

--- src/main/java/com/example/blog/domain/articles/ArticleRepository.java
+++ src/main/java/com/example/blog/domain/articles/ArticleRepository.java
@@ -3,8 +3,6 @@ package com.example.blog.domain.articles;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
-import java.util.List;
-
 @Component
 public interface ArticleRepository {
     Article save(Article article);
