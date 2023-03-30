7077a1f1d4872a3ea6a22327853dfd4e1d8dd4cd
kexin.an <kexin.an@thoughtworks.com>
2023-03-30 10:30:40 +0800

    test:finish ArticleControllerTest

---
+++ src/test/resources/scripts/insert_articles.sql
@@ -0,0 +1,5 @@
+INSERT INTO `articles` (`title`, `content`, `word_numbers`, `cover_url`, `created_at`, `updated_at`, `created_by`, `updated_by`, `deleted`, `version`)
+VALUES
+    ('article1', 'article test 1', 12, 'https://example.com/cover1.jpg', now(), now(), 'libingbing', 'libingbing', 0, 1),
+    ('article2', 'article test 222', 14, 'https://example.com/cover1.jpg', now(), now(), 'libingbing', 'libingbing', 0, 1),
+    ('article3', 'article test 33333', 16, 'https://example.com/cover1.jpg', now(), now(), 'libingbing', 'libingbing', 0, 1);
